c50c9373968b07d49b9de20854aee6da109fd16f
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-07-12 00:28:24 -0600

    circle class getters and setters example

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your Circle class here
+class Circle{
+    constructor(radi)
+    {
+        if (radi < 0) throw "illegal value found and used for the radius!";
+        this.radius = radi;
+    }
+
+    get diameter()
+    {
+        return 2 * this.radius;
+    }
+
+    set diameter(newdiam)
+    {
+        if (newdiam < 0) throw "illegal value found and used for the new radius!";
+        this.radius = newdiam / 2;
+    }
+
+    get area()
+    {
+        return Math.PI * this.radius * this.radius;
+    }
+
+    set area(nwarea)
+    {
+        if (nwarea < 0) throw "illegal value found and used for the new radius!";
+        this.radius = Math.sqrt(nwarea / Math.PI);
+    }
+
+    get circumference()
+    {
+        return Math.PI * this.diameter;
+    }
+
+    set circumference(nwcirc)
+    {
+        this.diameter = (nwcirc / Math.PI);
+    }
+}
